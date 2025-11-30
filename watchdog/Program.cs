251efea23174b7d11621aa53bc4bed0245843e1f
Max <max@MacBook-Air-Max.local>
2025-11-30 23:35:58 +0700

    watchdog + new struct directory

---
+++ watchdog/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+try {
+    var mmf = MemoryMappedFile.OpenExisting("heartbeat");
+    using var accessor = mmf.CreateViewAccessor();
+    byte value = accessor.ReadByte(0);
+
+    if (value == 1) {
+        accessor.Write(0, (byte)0);
+        Environment.Exit(0);
+    }
+    else {
+        Environment.Exit(1);
+    }
+}
+catch {
+    Environment.Exit(1);
+}
