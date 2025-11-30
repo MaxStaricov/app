5e49f74883eca398ab4d27910620438b6fac184d
Max <max@MacBook-Air-Max.local>
2025-11-30 09:06:50 +0700

    UseUrls()

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 app.MapGet("/echo/{message}", (string message) =>
 {
     return Results.Json(new { echo = message });
